d57935b216ebb3a6a3e452c2a923d350e34394f7
Nobody-IArc <utk3970@naver.com>
2025-05-12 21:45:31 +0900

    리액트 동작 구조 확인 및 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import React from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    let title = "To Do List"
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // const divStyle: React.CSSProperties = {
+    //     backgroundColor: 'rebeccapurple',
+    //     color: 'lightyellow',
+    //     fontSize: '2em',
+    //     textAlign: 'center'
+    // };
+
+    return (
+        <div className='title'>
+            <div>
+                ID<br></br>
+                <input type = 'text'></input>
+            </div>
+            <div>
+                Password<br></br>
+                <input type = 'password'></input>
+            </div>
+            {/*JSX 에서 주석을 사용하는 방법*/}
+        {/*<div style = {divStyle}>*/}
+            <h1>{title}</h1>
+        </div>
+    );
 }
 
-export default App
+export default App;
