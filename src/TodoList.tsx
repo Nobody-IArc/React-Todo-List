7468db31f2f08e5124691f7dfccb9a15a9d99782
Nobody-IArc <utk3970@naver.com>
2025-05-13 13:04:54 +0900

    할일 목록 상태화

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,15 +1,60 @@
 import * as React from "react";
 import './App.css';
+import {useState} from "react";
 
-interface MyComponentProps {
-    title: string;
+type Todo = {
+    id: number;
+    text: string;
+    isDone: boolean;
 }
 
-const TodoList: React.FC<MyComponentProps> = ({title}) => {
+// interface MyComponentProps {
+//     title: string;
+// }
+
+// const TodoList: React.FC<MyComponentProps> = ({title}) => {
+//     return (
+//         <div className = 'container'>
+//             <h1>{title}</h1>
+//         </div>
+//     )
+// }
+
+const TodoList: React.FC = () => {
+    const [todos, setTodos] = useState<Todo[]>([
+        { id: 1, text: 'Coffee', isDone: false },
+        { id: 2, text: 'Game', isDone: false },
+        { id: 3, text: 'Study', isDone: false },
+    ]);
     return (
-        <div className = 'container'>
-            <h1>{title}</h1>
+        <div>
+            <h1 className='titlebk'>Today</h1><p></p>
+            <div className='container'>
+                <div className='board'>
+                    <ul>
+                        <li>{todos[0].text}</li>
+                        <li>{todos[1].text}</li>
+                        <li>{todos[2].text}</li>
+                    </ul>
+                </div>
+            </div>
         </div>
+        // <div className = "container">
+        //     <h1>Today</h1>
+        //     <div>
+        //         <ul>
+        //             <li>
+        //                 {todos[0]}
+        //             </li>
+        //             <li>
+        //                 {todos[1]}
+        //             </li>
+        //             <li>
+        //                 {todos[2]}
+        //             </li>
+        //         </ul>
+        //     </div>
+        // </div>
     )
 }
 
