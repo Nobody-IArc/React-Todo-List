836dbbb1d83513c4e1eb9902759edca803bf8ad2
Nobody-IArc <utk3970@naver.com>
2025-05-13 13:11:43 +0900

    구조 분해 할당

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,47 @@ import TodoList from "./TodoList.tsx";
 // import FuncCom from './FuncCom.tsx';
 
 const App: React.FC = () => {
+    // 예시 객체
+    const person = {
+        name: '1ch',
+        age: 30,
+        height: 160
+    };
+
+    // 일반적 방식
+    /*
+    const name = person.name;
+    const age = person.age;
+    const height = person.height;
+     */
+
+    // 구조 분해 할당 방식 - 인덱스 별로 값 할당
+    const { name, age, height } = person;
+
+    console.log(name);
+    console.log(age);
+    console.log(height);
     // let title = "To Do List"
+
+    // 일반적 방식
+    /*
+    const colors = ['red', 'green', 'blue'];
+    const red = colors[0];
+    const green = colors[1];
+    const blue = colors[2];
+
+    console.log(red);
+    console.log(green);
+    console.log(blue);
+    */
+
+    // 구조 분해 할당 (Destructing)
+    const colors = ['red', 'green', 'blue'];
+    const [red, green, blue] = colors;
+
+    console.log(red);
+    console.log(green);
+    console.log(blue);
 
     return (
         <div>
